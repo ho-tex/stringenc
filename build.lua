1255b29083ecf5503d0a065b6a84db3f9bd2b3ea
David Carlisle <d.p.carlisle@gmail.com>
2019-11-28 10:07:22 +0000

    initial setup

---
+++ build.lua
@@ -0,0 +1,93 @@
+#!/usr/bin/env texlua
+
+
+module = "stringenc"
+
+textfiles = {"README.md"}
+unpackfiles = {'stringenc.dtx'}
+
+
+tdslocations={
+"doc/latex/oberdiek/stringenc.pdf",
+"source/latex/oberdiek/stringenc.dtx",
+"tex/generic/oberdiek/stringenc.sty",
+"tex/generic/oberdiek/se-ascii-print.def",
+"tex/generic/oberdiek/se-ascii.def",
+"tex/generic/oberdiek/se-clean7bit.def",
+"tex/generic/oberdiek/se-cp1250.def",
+"tex/generic/oberdiek/se-cp1251.def",
+"tex/generic/oberdiek/se-cp1252.def",
+"tex/generic/oberdiek/se-cp1257.def",
+"tex/generic/oberdiek/se-cp437.def",
+"tex/generic/oberdiek/se-cp850.def",
+"tex/generic/oberdiek/se-cp852.def",
+"tex/generic/oberdiek/se-cp855.def",
+"tex/generic/oberdiek/se-cp858.def",
+"tex/generic/oberdiek/se-cp865.def",
+"tex/generic/oberdiek/se-cp866.def",
+"tex/generic/oberdiek/se-dec-mcs.def",
+"tex/generic/oberdiek/se-iso-8859-1.def",
+"tex/generic/oberdiek/se-iso-8859-10.def",
+"tex/generic/oberdiek/se-iso-8859-11.def",
+"tex/generic/oberdiek/se-iso-8859-13.def",
+"tex/generic/oberdiek/se-iso-8859-14.def",
+"tex/generic/oberdiek/se-iso-8859-15.def",
+"tex/generic/oberdiek/se-iso-8859-16.def",
+"tex/generic/oberdiek/se-iso-8859-2.def",
+"tex/generic/oberdiek/se-iso-8859-3.def",
+"tex/generic/oberdiek/se-iso-8859-4.def",
+"tex/generic/oberdiek/se-iso-8859-5.def",
+"tex/generic/oberdiek/se-iso-8859-6.def",
+"tex/generic/oberdiek/se-iso-8859-7.def",
+"tex/generic/oberdiek/se-iso-8859-8.def",
+"tex/generic/oberdiek/se-iso-8859-9.def",
+"tex/generic/oberdiek/se-koi8-r.def",
+"tex/generic/oberdiek/se-mac-centeuro.def",
+"tex/generic/oberdiek/se-mac-cyrillic.def",
+"tex/generic/oberdiek/se-mac-roman.def",
+"tex/generic/oberdiek/se-nextstep.def",
+"tex/generic/oberdiek/se-pdfdoc.def",
+"tex/generic/oberdiek/se-utf16le.def",
+"tex/generic/oberdiek/se-utf32be.def",
+"tex/generic/oberdiek/se-utf32le.def",
+"tex/generic/oberdiek/se-utf8.def"
+}
+ 
+packtdszip  = true
+
+maxprintline=10000
+checkruns = 2
+
+
+
+function update_tag(file,content,tagname,tagdate)
+
+local tagpattern="(%d%d%d%d[-/]%d%d[-/]%d%d) v(%d+[.])(%d+)"
+local oldv,newv
+if tagname == 'auto' then
+  local i,j,olddate,a,b
+  i,j,olddate,a,b= string.find(content, tagpattern)
+  if i == nil then
+    print('OLD TAG NOT FOUND')
+    return content
+  else
+    print ('FOUND: ' .. olddate .. ' v' .. a .. b )
+    oldv = olddate .. ' v' .. a .. b
+    newv = tagdate .. ' v'  .. a .. math.floor(b + 1)
+    print('USING OLD TAG: ' .. oldv)
+    print('USING NEW TAG: ' .. newv)
+    content=string.gsub(content,"{Version}{" .. oldv,'##OLDV##')
+    content=string.gsub(content,oldv,newv)
+    content=string.gsub(content,'##OLDV##',"{Version}{" .. oldv)
+    content = string.gsub(content,
+        '%% \\end{History}',
+	'%%   \\begin{Version}{' .. newv .. '}\n%%   \\item Updated\n%%   \\end{Version}\n%% \\end{History}')
+    return content
+  end
+else
+  error("only automatic tagging supported")
+end
+
+end
+
+
